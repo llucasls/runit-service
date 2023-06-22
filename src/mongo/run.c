1bd7f11c4fdf9d0cdb40d629a518129b5f714b77
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-06-22 01:30:30 -0300

    create mongo service
    Create service to run MongoDB in a Docker container.

---
+++ src/mongo/run.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+int main() {
+	char compose_file_path[256];
+	const char* home_dir = getenv("HOME");
+
+	if (home_dir == NULL) {
+		fprintf(stderr, "Error: HOME environment variable not set\n");
+		return 1;
+	}
+
+	snprintf(compose_file_path, sizeof(compose_file_path), "%s/docker/mongo/mongo.yml", home_dir);
+
+	char* const argv[] = { "/usr/bin/docker-compose", "-f", compose_file_path, "up", NULL };
+	execv("/usr/bin/docker-compose", argv);
+
+	perror("mongo");
+	return 1;
+}
