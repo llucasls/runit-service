c4a10232937b5158003f66293b7730e15188c89b
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-06-30 04:47:43 -0300

    refactor: change into HOME before starting Emacs

--- src/emacs/run.c
+++ src/emacs/run.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 int main() {
+	char *home_dir = getenv("HOME");
+	int chdir_status = chdir(home_dir);
+
+	if (chdir_status != 0) {
+		perror("failed to change directory");
+		return 1;
+	}
+
 	char *args[] = {"emacs", "--fg-daemon", NULL};
 	execv("/usr/bin/emacs", args);
 
